a768d5904c0f531cd5014d9f119e4fc4676b7b8a
Matus Chochlik <chochlik@gmail.com>
2022-04-16 20:25:27 +0200

    Additions to openra map (volcano)

--- openra/src/maps/ra/volcano/volcano.lua
+++ openra/src/maps/ra/volcano/volcano.lua
@@ -3,32 +3,65 @@ SpawnCreeps = function()
     local neutral = Player.GetPlayer("Neutral")
 
     local spawninfo = {
-        {Temple, CreepSpawnA, CreepDestA},
-        {Temple, CreepSpawnB, CreepDestB},
-        {Temple, CreepSpawnC, CreepDestC}
+        {CreepSpawnA, CreepDestA},
+        {CreepSpawnB, CreepDestB},
+        {CreepSpawnC, CreepDestC}
     }
     local spawn = Utils.Random(spawninfo)
 	local what = "spider"
 
     local pop = creeps.GetActorsByType(what)
-    if #pop < 100 and not spawn[1].IsDead and spawn[1].EffectiveOwner == neutral
+    if #pop < 100 and not Temple.IsDead and Temple.EffectiveOwner == neutral
 	then
         Utils.Do(
             Reinforcements.Reinforce(
                 creeps,
                 {what, what},
-                {spawn[2].Location, spawn[3].Location}
+                {spawn[1].Location, spawn[1].Location}
             ),
             function(z) Trigger.OnIdle(z, z.Hunt) end
         )
     end
 
     Trigger.AfterDelay(
-        DateTime.Seconds(Utils.RandomInteger(30, 45)),
+        DateTime.Seconds(Utils.RandomInteger(60, 90)),
         SpawnCreeps
     )
 end
 
+SpawnWizards = function()
+    local creeps = Player.GetPlayer("Creeps")
+    local neutral = Player.GetPlayer("Neutral")
+
+    local spawninfo = {
+        {CreepSpawnA, CreepDestA},
+        {CreepSpawnB, CreepDestB},
+        {CreepSpawnC, CreepDestC}
+    }
+    local spawn = Utils.Random(spawninfo)
+	local what = "wzrd"
+
+    local pop = creeps.GetActorsByType(what)
+    if #pop < 1 and not Temple.IsDead and Temple.EffectiveOwner == neutral
+	then
+        Utils.Do(
+            Reinforcements.Reinforce(
+                creeps,
+                {what},
+                {spawn[1].Location, spawn[1].Location}
+            ),
+            function(z) Trigger.OnIdle(z, z.Hunt) end
+        )
+    end
+
+    Trigger.AfterDelay(
+        DateTime.Seconds(Utils.RandomInteger(450, 500)),
+        SpawnCreeps
+    )
+end
+
+
 WorldLoaded = function()
     Trigger.AfterDelay(DateTime.Seconds(60), SpawnCreeps)
+    Trigger.AfterDelay(DateTime.Seconds(150), SpawnWizards)
 end
