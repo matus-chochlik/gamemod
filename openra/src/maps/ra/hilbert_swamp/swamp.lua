1262860a33a440e6fb1b1882183cccf9bc913bfc
Matus Chochlik <chochlik@gmail.com>
2023-02-21 20:32:53 +0100

    Additions to the hilbert swamp map

--- openra/src/maps/ra/hilbert_swamp/swamp.lua
+++ openra/src/maps/ra/hilbert_swamp/swamp.lua
@@ -112,6 +112,48 @@ SpawnMilitaryRaid = function()
     )
 end
 
+SpawnMonolithRaid = function()
+    local monolith = Player.GetPlayer("Monolith")
+
+    local spawnpoints = {
+		EntryNE, EntryNNE1, EntrySE3
+    }
+    local droppoints = {
+		Landing01, Landing02, Landing03, Landing04, Landing05, Landing06,
+		Landing07, Landing08, Landing09, Landing10, Landing11, Landing12,
+		Landing13, Landing14, Landing15, Landing16, Landing17, Landing18,
+		Landing20, Landing21, Landing22, Landing23, Landing24, Landing25,
+		Landing26, Landing27, Landing28, Landing29, Landing30, Landing31,
+		Landing32, Landing33, Landing34, Landing35, Landing36, Landing37,
+		Landing38, Landing39, Landing40, Landing41, Landing42, Landing43,
+		Landing44, Landing45, Landing46, Landing47, Landing48, Landing49,
+		Landing50, Landing51, Landing52, Landing53, Landing54,
+    }
+    local entry = Utils.Random(spawnpoints)
+    local start = Utils.Random(droppoints)
+    local exit = Utils.Random(spawnpoints)
+
+	Utils.Do(
+		Player.GetPlayers(function(p) return p.IsLocalPlayer end),
+		function(p)
+			Radar.Ping(p, Map.CenterOfCell(entry.Location), HSLColor.White, 200)
+		end
+	)
+
+	spawned = Reinforcements.ReinforceWithTransport(
+		monolith, "dthtrk",
+		{"e1", "e1", "e1", "e2", "e2", "e3", "e3", "e4", "sniper"},
+		{entry.Location, start.Location},
+		{start.Location, exit.Location}
+	)
+	StartHuntParty(spawned[2])
+
+    Trigger.AfterDelay(
+        DateTime.Seconds(Utils.RandomInteger(50, 100)),
+        SpawnMonolithRaid
+    )
+end
+
 SpawnCreeps = function()
     local creeps = Player.GetPlayer("Creeps")
 
@@ -167,6 +209,7 @@ SpawnCreeps = function()
 end
 
 WorldLoaded = function()
+    Trigger.AfterDelay(DateTime.Seconds(110), SpawnMonolithRaid)
     Trigger.AfterDelay(DateTime.Seconds(120), SpawnMilitaryRaid)
     Trigger.AfterDelay(DateTime.Seconds(35), SpawnCreeps)
 end
