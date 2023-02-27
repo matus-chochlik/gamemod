7551b50873a1b84ad106de1dafdc761d8f66c09c
Matus Chochlik <chochlik@gmail.com>
2023-02-27 06:43:29 +0100

    Additions and changes to the hilbert swamp map

--- openra/src/maps/ra/hilbert_swamp/swamp.lua
+++ openra/src/maps/ra/hilbert_swamp/swamp.lua
@@ -1,39 +1,45 @@
 SetupRaidActors = function(units)
-    local leader = units[1]
+	units = Utils.Where(
+		units,
+		function(a)
+			return not a.IsDead
+		end
+	)
+	local leader = units[1]
 
-    Utils.Do(
-        units,
-        function(a)
-            if a == leader then
-                Trigger.OnIdle(a, a.Hunt)
-            else
-                Trigger.OnIdle(
-                    a,
-                    function()
-                        if leader.IsDead then
-                            a.Hunt()
-                        else
-                            a.Guard(leader)
-                        end
-                    end
-                )
-            end
-        end
-    )
+	Utils.Do(
+		units,
+		function(a)
+			if a == leader then
+				Trigger.OnIdle(a, a.Hunt)
+			else
+				Trigger.OnIdle(
+					a,
+					function()
+						if not leader.IsDead then
+							a.Guard(leader)
+						else
+							a.Hunt()
+						end
+					end
+				)
+			end
+		end
+	)
 
 end
 
 SpawnMilitaryRaid = function()
-    local military = Player.GetPlayer("Military")
+	local military = Player.GetPlayer("Military")
 
-    local spawnpoints = {
+	local spawnpoints = {
 		EntryNNE1, EntryNNE2, EntryN1, EntryN2, EntryNNW1, EntryNNW2,
 		EntryNW2, EntryNW1, EntryNNW3, EntryNW2, EntryNW1, EntryE, EntryW,
 		EntrySW2, EntrySW1, EntrySW3, EntrySSW2, EntrySSW1, EntrySW1,
 		EntryS, EntrySE1, EntrySE2, EntrySSE1, EntryE, EntryNE1,
 		EntryNNE1, EntryNE, EntryNE1,
-    }
-    local landings = {
+	}
+	local landings = {
 		Landing01, Landing02, Landing03, Landing04, Landing05, Landing06,
 		Landing07, Landing08, Landing09, Landing10, Landing11, Landing12,
 		Landing13, Landing14, Landing15, Landing16, Landing17, Landing18,
@@ -43,10 +49,10 @@ SpawnMilitaryRaid = function()
 		Landing38, Landing39, Landing40, Landing41, Landing42, Landing43,
 		Landing44, Landing45, Landing46, Landing47, Landing48, Landing49,
 		Landing50, Landing51, Landing52, Landing53, Landing54,
-    }
-    local entry = Utils.Random(spawnpoints)
-    local landing = Utils.Random(landings)
-    local exit = Utils.Random(spawnpoints)
+	}
+	local entry = Utils.Random(spawnpoints)
+	local landing = Utils.Random(landings)
+	local exit = Utils.Random(spawnpoints)
 
 	Utils.Do(
 		Player.GetPlayers(function(p) return p.IsLocalPlayer end),
@@ -104,19 +110,19 @@ SpawnMilitaryRaid = function()
 		end
 	)
 
-    Trigger.AfterDelay(
-        DateTime.Seconds(Utils.RandomInteger(60, 120)),
-        SpawnMilitaryRaid
-    )
+	Trigger.AfterDelay(
+		DateTime.Seconds(Utils.RandomInteger(60, 120)),
+		SpawnMilitaryRaid
+	)
 end
 
-SpawnMonolithRaid = function()
-    local monolith = Player.GetPlayer("Monolith")
+SpawnLoners = function()
+	local loners = Player.GetPlayer("Loners")
 
-    local spawnpoints = {
+	local spawnpoints = {
 		EntryNE, EntryNNE1, EntrySE3
-    }
-    local droppoints = {
+	}
+	local droppoints = {
 		Landing01, Landing02, Landing03, Landing04, Landing05, Landing06,
 		Landing07, Landing08, Landing09, Landing10, Landing11, Landing12,
 		Landing13, Landing14, Landing15, Landing16, Landing17, Landing18,
@@ -126,10 +132,105 @@ SpawnMonolithRaid = function()
 		Landing38, Landing39, Landing40, Landing41, Landing42, Landing43,
 		Landing44, Landing45, Landing46, Landing47, Landing48, Landing49,
 		Landing50, Landing51, Landing52, Landing53, Landing54,
-    }
-    local entry = Utils.Random(spawnpoints)
-    local start = Utils.Random(droppoints)
-    local exit = Utils.Random(spawnpoints)
+	}
+	local entry = Utils.Random(spawnpoints)
+	local start = Utils.Random(droppoints)
+	local exit = Utils.Random(spawnpoints)
+
+	spawned = Reinforcements.ReinforceWithTransport(
+		loners, "apc",
+		{"sniper", "e1", "e2", "e1", "e3"},
+		{entry.Location, start.Location},
+		{start.Location, exit.Location}
+	)
+	SetupRaidActors(spawned[2])
+
+	Trigger.AfterDelay(
+		DateTime.Seconds(Utils.RandomInteger(30, 50)),
+		SpawnLoners
+	)
+end
+
+band = {}
+
+SpawnBandits = function()
+	local bandits = Player.GetPlayer("Bandits")
+
+	if Utils.RandomInteger(0, 2) == 0
+	then
+		local spawnpoints = {
+			EntryNE, EntryNNE1, EntrySE3
+		}
+		local droppoints = {
+			BanditBase1
+		}
+		local entry = Utils.Random(spawnpoints)
+		local start = Utils.Random(droppoints)
+		local exit = Utils.Random(spawnpoints)
+
+		spawned = Reinforcements.ReinforceWithTransport(
+			bandits, "jeep",
+			{"e1", Utils.Random({"e1", "e2", "e3"})},
+			{entry.Location, start.Location},
+			{start.Location, exit.Location}
+		)
+	else
+		local spawnpoints = {
+			EntryNE1, EntryE, EntrySSE1, EntrySE2
+		}
+		local droppoints = {
+			BanditLanding1
+		}
+		local entry = Utils.Random(spawnpoints)
+		local start = Utils.Random(droppoints)
+		local exit = Utils.Random(spawnpoints)
+
+		spawned = Reinforcements.ReinforceWithTransport(
+			bandits, "spdbt",
+			{"e1", Utils.Random({"e1", "e2", "e3"})},
+			{entry.Location, start.Location},
+			{start.Location, exit.Location}
+		)
+	end
+	Utils.Do(
+		spawned[2],
+		function(bandit)
+			table.insert(band, bandit)
+		end
+	)
+
+	if #band > 20 then
+		SetupRaidActors(band)
+		band = {}
+	end
+	
+
+	Trigger.AfterDelay(
+		DateTime.Seconds(Utils.RandomInteger(15, 25)),
+		SpawnBandits
+	)
+end
+
+SpawnMonolithRaid = function()
+	local monolith = Player.GetPlayer("Monolith")
+
+	local spawnpoints = {
+		EntryNE, EntryNNE1, EntrySE3
+	}
+	local droppoints = {
+		Landing01, Landing02, Landing03, Landing04, Landing05, Landing06,
+		Landing07, Landing08, Landing09, Landing10, Landing11, Landing12,
+		Landing13, Landing14, Landing15, Landing16, Landing17, Landing18,
+		Landing20, Landing21, Landing22, Landing23, Landing24, Landing25,
+		Landing26, Landing27, Landing28, Landing29, Landing30, Landing31,
+		Landing32, Landing33, Landing34, Landing35, Landing36, Landing37,
+		Landing38, Landing39, Landing40, Landing41, Landing42, Landing43,
+		Landing44, Landing45, Landing46, Landing47, Landing48, Landing49,
+		Landing50, Landing51, Landing52, Landing53, Landing54,
+	}
+	local entry = Utils.Random(spawnpoints)
+	local start = Utils.Random(droppoints)
+	local exit = Utils.Random(spawnpoints)
 
 	Utils.Do(
 		Player.GetPlayers(function(p) return p.IsLocalPlayer end),
@@ -146,20 +247,20 @@ SpawnMonolithRaid = function()
 	)
 	SetupRaidActors(spawned[2])
 
-    Trigger.AfterDelay(
-        DateTime.Seconds(Utils.RandomInteger(50, 100)),
-        SpawnMonolithRaid
-    )
+	Trigger.AfterDelay(
+		DateTime.Seconds(Utils.RandomInteger(50, 100)),
+		SpawnMonolithRaid
+	)
 end
 
 SpawnCreeps = function()
-    local creeps = Player.GetPlayer("Creeps")
+	local creeps = Player.GetPlayer("Creeps")
 
-    local spawnpoints = {
+	local spawnpoints = {
 		CreepSpawn01, CreepSpawn02, CreepSpawn03,
 		CreepSpawn04, CreepSpawn05, CreepSpawn06
-    }
-    local huntpoints = {
+	}
+	local huntpoints = {
 		Landing01, Landing02, Landing03, Landing04, Landing05, Landing06,
 		Landing07, Landing08, Landing09, Landing10, Landing11, Landing12,
 		Landing13, Landing14, Landing15, Landing16, Landing17, Landing18,
@@ -169,14 +270,14 @@ SpawnCreeps = function()
 		Landing38, Landing39, Landing40, Landing41, Landing42, Landing43,
 		Landing44, Landing45, Landing46, Landing47, Landing48, Landing49,
 		Landing50, Landing51, Landing52, Landing53, Landing54,
-    }
-    local entry = Utils.Random(spawnpoints)
-    local start = Utils.Random(huntpoints)
+	}
+	local entry = Utils.Random(spawnpoints)
+	local start = Utils.Random(huntpoints)
 
 	if Utils.RandomInteger(0, 2) == 0
 	then
 		local pop = creeps.GetActorsByType("dog")
-		if #pop < 120 then
+		if #pop < 150 then
 			Utils.Do(
 				Reinforcements.Reinforce(
 					creeps,
@@ -200,14 +301,16 @@ SpawnCreeps = function()
 		end
 	end
 
-    Trigger.AfterDelay(
-        DateTime.Seconds(Utils.RandomInteger(30, 45)),
-        SpawnCreeps
-    )
+	Trigger.AfterDelay(
+		DateTime.Seconds(Utils.RandomInteger(20, 40)),
+		SpawnCreeps
+	)
 end
 
 WorldLoaded = function()
-    Trigger.AfterDelay(DateTime.Seconds(110), SpawnMonolithRaid)
-    Trigger.AfterDelay(DateTime.Seconds(120), SpawnMilitaryRaid)
-    Trigger.AfterDelay(DateTime.Seconds(35), SpawnCreeps)
+	Trigger.AfterDelay(DateTime.Seconds(110), SpawnMonolithRaid)
+	Trigger.AfterDelay(DateTime.Seconds(120), SpawnMilitaryRaid)
+	Trigger.AfterDelay(DateTime.Seconds(35), SpawnCreeps)
+	Trigger.AfterDelay(DateTime.Seconds(15), SpawnLoners)
+	Trigger.AfterDelay(DateTime.Seconds(10), SpawnBandits)
 end
